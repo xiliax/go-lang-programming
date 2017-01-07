06499e6a3f9719abe2038895b3d6ae82f23805d4
Verrol Adams <another@xiliax.com>
2017-01-07 07:49:29 -0500

    c03s08 - Slice Spread Operator (...)

---
+++ c03-Arrays-and-Slices/c03s08/stub/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Understanding the 'spread operator', ...")
+	var s0 = []int{11, 56, 37, 89, 23, 101}
+
+	numberStats(11)
+	numberStats(11, 56)
+	numberStats(11, 89, 23)
+	numberStats(s0...)
+}
+
+func printNumbers(a ...int) {
+	for i := 0; i < len(a); i++ {
+		fmt.Println(a[i])
+	}
+}
+
+// numberStats - print some stats on the numbers provided
+func numberStats(a ...int) {
+	// TODO - 1 : Print sum of number
+
+	// TODO - 2 : Print average of number
+
+	// TODO - 3 : Print smallest number
+
+	// TODO - 4 : Print the lastest number
+}
